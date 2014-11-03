51d9ea80e046d734159d67ec151c3bf70e69df31
Ale1391 <alejandro.m.napoli@gmail.com>
2014-11-03 02:19:39 -0300

    Revert "Nuevo Boton."
    This reverts commit 58fec08c5faf9de5ed3f5a0c134af4b571efeac7.

--- FrbaHotel/FrbaHotel/Principal.Designer.cs
+++ FrbaHotel/FrbaHotel/Principal.Designer.cs
@@ -29,33 +29,22 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(172, 152);
+            this.button1.Location = new System.Drawing.Point(110, 152);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Aceptar";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(49, 152);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Cancelar";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 273);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Principal";
             this.Text = "Principal";
@@ -66,7 +55,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
